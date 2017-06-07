67261cfdd0ccae96c678dc8f1256e1d9d20ac89c
Sean McLeod <mcleodseanm@gmail.com>
2017-06-07 13:56:42 +0000

    <Initial Commit>

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include "MainHandler.h"
+
+//Create MainHandler object and call it run run the main execution of the program
+
+//literally nothing else
+
+//maybe infinitely far in the future will also use it to show a splash screen
+//...if I ever get to GUI
